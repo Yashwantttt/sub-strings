06ab1551e8ec74e71cec7058a559482cf0b0ce6a
Yashwantttt <dangydhere@gmail.com>
2025-01-11 16:04:47 +0530

    All logic written

--- ruby-code.rb
+++ ruby-code.rb
@@ -10,13 +10,23 @@ def substrings(word,array)
     print final_hash
     
   else
-    puts "Its multiple word you idiot"
+    words = word.split
+    words.each do |word|
+      match = array.select do |array_element|
+        word.include?(array_element)
+      end
+      final_hash = match.reduce(Hash.new(0)) do |result,same|
+        result[same] += 1
+        result
+      end
+      print final_hash
+    end
   end
 end
 
 array1 = ["Hello" ,"guys","guys","guy"]
 
-substrings("guys",array1)
+substrings("Hello guys",array1)
 
 
 #to make it case insensitive ...we just create 2 new variable ...one of array and one of word then 
